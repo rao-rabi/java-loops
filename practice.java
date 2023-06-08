6495160c1b799e8d90a5fe77dfc8ac21e16678f1
rao rabi <rabirao1039@gmail.com>
2023-06-08 22:35:44 +0500

    first commit

---
+++ practice.java
@@ -0,0 +1,22 @@
+// import java.util.Scanner;
+
+// public class practice {
+//     public static void main(String[] args) {
+//         //sum of first n natural numbers
+//         System.out.print("Enter a number: ");
+//         Scanner sc = new Scanner(System.in);
+//         int num = sc.nextInt();
+//         int sum = 0;
+//         for (int i = 1; i<=num; i++){
+//             sum = sum+=i;
+//         }
+//         System.out.println("Sum of first natural numbers till "+ num +" is: " +sum);
+
+//         //print the table of number input by user.
+//         System.out.print("Enter a number to get its table: ");
+//         int numTable = sc.nextInt();
+//         for(int i = 1; i<=10; i++){
+//             System.out.println(numTable + " x " + i + " = " + numTable*i);
+//         }
+//     }
+// }
