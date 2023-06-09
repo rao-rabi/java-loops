5dd094666ee4faaa5427a7bc43ba65a97692e347
rao rabi <rabirao1039@gmail.com>
2023-06-09 21:31:31 +0500

    new changes done

--- loops.java
+++ loops.java
@@ -1,20 +1,20 @@
-// public class loops {
-//     public static void main(String[] args) {
-//     //for loop
-//         for(int i = 0; i<=10; i++ ){
-//             System.out.print(i + " ");
-//         }
-//     //while loop    
-//         int j= 0;
-//         while(j<11){
-//             System.out.println(j);
-//             j++;
-//         }
-//     //dowhile loop    
-//         int z = 0;
-//         do {
-//             System.out.print(z + " ");
-//             z++;
-//         } while (z<11);
-//     }
-// }
+public class loops {
+    public static void main(String[] args) {
+    //for loop
+        for(int i = 0; i<=10; i++ ){
+            System.out.print(i + " ");
+        }
+    //while loop    
+        int j= 0;
+        while(j<11){
+            System.out.println(j);
+            j++;
+        }
+    //dowhile loop    
+        int z = 0;
+        do {
+            System.out.print(z + " ");
+            z++;
+        } while (z<11);
+    }
+}
--- pattern.java
+++ pattern.java
@@ -1,91 +1,91 @@
-// public class pattern {
-//     public static void main(String[] args) {
-//         // print the pattern of solid rectangle of stars
-//         // *****
-//         // *****
-//         // *****
-//         // *****
-//         System.out.println("The solid rectangle pattern is as follows: ");
-//         for (int i = 1; i <= 4; i++) {
-//             for (int j = 1; j <= 5; j++) {
-//                 System.out.print(" * ");
-//             }
-//             System.out.println();
-//         }
+public class pattern {
+    public static void main(String[] args) {
+        // print the pattern of solid rectangle of stars
+        // *****
+        // *****
+        // *****
+        // *****
+        System.out.println("The solid rectangle pattern is as follows: ");
+        for (int i = 1; i <= 4; i++) {
+            for (int j = 1; j <= 5; j++) {
+                System.out.print(" * ");
+            }
+            System.out.println();
+        }
 
-//         // print the pattern of hollow rectangle of stars
-//         // *****
-//         // *   *
-//         // *   *
-//         // *****
-//         System.out.println("The solid rectangle hollow pattern is as follows: ");
-//         for (int i = 1; i <= 4; i++) {
-//             for (int j = 1; j <= 5; j++) {
-//                 if (i == 1 || j == 1 || i == 4 || j == 5) {
-//                     System.out.print(" * ");
-//                 } else {
-//                     System.out.print("   ");
-//                 }
-//             }
-//             System.out.println();
-//         }
+        // print the pattern of hollow rectangle of stars
+        // *****
+        // *   *
+        // *   *
+        // *****
+        System.out.println("The solid rectangle hollow pattern is as follows: ");
+        for (int i = 1; i <= 4; i++) {
+            for (int j = 1; j <= 5; j++) {
+                if (i == 1 || j == 1 || i == 4 || j == 5) {
+                    System.out.print(" * ");
+                } else {
+                    System.out.print("   ");
+                }
+            }
+            System.out.println();
+        }
 
-//         // print the patttern of half pyramid
-//         // *
-//         // **
-//         // ***
-//         // ****
-//         System.out.println("The half pyramid pattern is as follows: ");
-//         for (int i = 1; i <= 4; i++) {
-//             for (int j = 1; j <= i; j++) {
-//                 System.out.print(" * ");
-//             }
-//             System.out.println();
-//         }
+        // print the patttern of half pyramid
+        // *
+        // **
+        // ***
+        // ****
+        System.out.println("The half pyramid pattern is as follows: ");
+        for (int i = 1; i <= 4; i++) {
+            for (int j = 1; j <= i; j++) {
+                System.out.print(" * ");
+            }
+            System.out.println();
+        }
 
-//         // print the reverse patttern of half pyramid
-//         // ****
-//         // ***
-//         // **
-//         // *
+        // print the reverse patttern of half pyramid
+        // ****
+        // ***
+        // **
+        // *
 
-//         System.out.println("The half reverse pyramid pattern is as follows: ");
-//         for (int i = 4; i >= 1; i--) {
-//             for (int j = 1; j <= i; j++) {
-//                 System.out.print(" * ");
-//             }
-//             System.out.println();
-//         }
+        System.out.println("The half reverse pyramid pattern is as follows: ");
+        for (int i = 4; i >= 1; i--) {
+            for (int j = 1; j <= i; j++) {
+                System.out.print(" * ");
+            }
+            System.out.println();
+        }
 
-//         // print the inverted patttern of half pyramid(180deg rotated)
-//         // *
-//         // **
-//         // ***
-//         // ****
+        // print the inverted patttern of half pyramid(180deg rotated)
+        // *
+        // **
+        // ***
+        // ****
 
-//         System.out.println("The half inverted pyramid pattern is as follows: ");
-//         for (int i = 1; i <= 4; i++) {
-//             for (int j = 1; j <= 4 - i; j++) {
-//                 System.out.print("   ");
-//             }
-//             for (int j = 1; j <= i; j++) {
-//                 System.out.print(" * ");
-//             }
-//             System.out.println();
-//         }
-//         // print the patttern
-//         // 1
-//         // 1 2
-//         // 1 2 3
-//         // 1 2 3 4
-//         // 1 2 3 4 5
+        System.out.println("The half inverted pyramid pattern is as follows: ");
+        for (int i = 1; i <= 4; i++) {
+            for (int j = 1; j <= 4 - i; j++) {
+                System.out.print("   ");
+            }
+            for (int j = 1; j <= i; j++) {
+                System.out.print(" * ");
+            }
+            System.out.println();
+        }
+        // print the patttern
+        // 1
+        // 1 2
+        // 1 2 3
+        // 1 2 3 4
+        // 1 2 3 4 5
 
-//         System.out.println("The pattern is as follows: ");
-//         for (int i = 1; i <= 5; i++) {
-//             for (int j = 1; j <= i; j++) {
-//                 System.out.print(" " + j + " ");
-//             }
-//             System.out.println();
-//         }
-//     }
-// }
+        System.out.println("The pattern is as follows: ");
+        for (int i = 1; i <= 5; i++) {
+            for (int j = 1; j <= i; j++) {
+                System.out.print(" " + j + " ");
+            }
+            System.out.println();
+        }
+    }
+}
